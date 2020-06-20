f309ebac878b18707df994d2e1fa7c6f7f74d1d2
Armen Baldryan <Baldryan_Armen@gmail.com>
2020-06-20 10:40:53 +0400

    Removing unnecessary type

--- src/types.d.ts
+++ src/types.d.ts
@@ -16,19 +16,8 @@ export interface DisabledValuesType {
   right: number;
 }
 
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      REACT_APP_SQUARE_SIZE: string;
-    }
-  }
-}
-
 declare var process: {
   env: {
     REACT_APP_SQUARE_SIZE: string;
   };
 };
-
-// export const REACT_APP_NOT_SECRET_CODE: string = process.env
-//   .REACT_APP_NOT_SECRET_CODE as string;
